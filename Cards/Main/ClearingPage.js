ea2a194f064a3540e17a4779e98d3a9e5278a94e
idan <72699248+idanbe11@users.noreply.github.com>
2022-08-24 00:01:38 +0300

    commit

--- Cards/Main/ClearingPage.js
+++ Cards/Main/ClearingPage.js
@@ -1,23 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, SafeAreaView,Button,Alert, TouchableHighlight,TouchableWithoutFeedback, TouchableOpacity , TextInput} from 'react-native';
-
+import React, { useState } from "react";
 import Clearing from './Icons/Clearing.png';
+import TranzillaLogo from './Icons/TranzillaLogo.jpg';
 
 
-export default function ClearingPage({text , onPress}) { 
+export default function ClearingPage({text , onPress ,item}) { 
     return (
       <SafeAreaView style={styles.body}>
         <Image source={Clearing} style={styles.ClearingBackground} />
             <View style={styles.container}>
                 <View style={styles.subContainer}>
-                    <Text>{item.title}</Text>
+                    <Text style={styles.mainTitle}>אפשרויות סליקה</Text>
                 </View>
+            <View style={styles.FirstBoard}>
+                <Image source={TranzillaLogo} style={styles.Logos} />
+                    <Text style={styles.text}>29 ש״ח בחודש +           
+החל מ-1.4% עמלת סליקה בכל עסקה</Text>
             </View>
-
-             
-      </SafeAreaView>
+            <View style={styles.SecondBoard}>
+                <Image source={TranzillaLogo} style={styles.Logos} />
+                    <Text style={styles.text}>29 ש״ח בחודש +           
+החל מ-1.4% עמלת סליקה בכל עסקה</Text>
+            </View>
+            </View>        
+        </SafeAreaView>
     );
-  }
+}
 
   const styles = StyleSheet.create({
     ClearingBackground:{
@@ -36,11 +45,67 @@ export default function ClearingPage({text , onPress}) {
         backgroundColor: "#e9f0f7",
     },
     subContainer: {
-        width:'90%',
-        height:160,
-        top:90,
-        left:20,
+        width:'88%',
+        height:70,
+        top:10,
+        left:25,
+        borderRadius:22,
+        backgroundColor: "#7471F2",
+        shadowColor: "#a9a9a9",
+        shadowOffset: {
+          width: 2,
+          height: 3,
+        },
+        shadowOpacity: 0.27,
+        shadowRadius: 4.65,
+    },
+    mainTitle: {
+        fontSize:30,
+        fontWeight:700,
+        textAlign:'center',
+        color:'white',
+        marginTop:10,
+    },
+    FirstBoard: {
+        width:'88%',
+        height:230,
+        top:30,
+        left:25,
         borderRadius:22,
         backgroundColor: "white",
+        shadowColor: "#a9a9a9",
+        shadowOffset: {
+          width: 2,
+          height: 3,
+        },
+        shadowOpacity: 0.27,
+        shadowRadius: 4.65,
+    },
+    Logos: {
+        width:'60%',
+        height:55,
+        top: 10,
+        left:80,
+    },
+    text: {
+        fontSize:22,
+        fontWeight:600,
+        marginTop:25,
+        marginRight:12,
+    },
+    SecondBoard: {
+        width:'88%',
+        height:230,
+        top:50,
+        left:25,
+        borderRadius:22,
+        backgroundColor: "white",
+        shadowColor: "#a9a9a9",
+        shadowOffset: {
+          width: 2,
+          height: 3,
+        },
+        shadowOpacity: 0.27,
+        shadowRadius: 4.65,
     },
   });
