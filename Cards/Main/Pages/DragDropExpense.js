de519fa7315b1e9aeabd512c93ce6d2123922542
idan <72699248+idanbe11@users.noreply.github.com>
2022-09-04 19:42:37 +0300

    commit

---
+++ Cards/Main/Pages/DragDropExpense.js
@@ -0,0 +1,173 @@
+import React, { useState } from "react";
+import {Text,View,StyleSheet,TouchableOpacity,UIManager,Dimensions,FlatList} from "react-native";
+import DraggableFlatList, { ScaleDecorator,RenderItemParams,} from "react-native-draggable-flatlist";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+
+
+const { width } = Dimensions.get("window");
+
+
+const list = [
+  {
+    id: "one",
+    cardname: "קבלות אחרונות שלי",
+  },
+  {
+    id: "two",
+    cardname: "לפי חודש ",
+    text:"ינואר",
+    text2:"פברואר",
+    text3:"מרץ",
+    text4:"אפריל",
+    May:"מאי",
+    Jun:"יוני",
+    Jul:"יולי",
+    Aug:"אוגוסט",
+    Sep:"ספטמבר",
+    Oct:"אוקטובר",
+    Nov:"נובמבר",
+    Dec:"דצמבר",
+  },
+  {
+    id: "three",
+    cardname: "לפי שנה",
+    text:"2019",
+    text2:"2020",
+    text3:"2021",
+    text4:"2022",
+  }
+];
+
+
+if (Platform.OS === "android") {
+  UIManager.setLayoutAnimationEnabledExperimental &&
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
+export default function DragDrop() {
+  const [data, setData] = useState(list);
+  const renderItem = ({ item, drag, isActive }) => {
+    return (
+      <ScaleDecorator style={styles.body}>
+          <TouchableOpacity 
+          activeOpacity={1} 
+          onLongPress={drag} 
+          disabled={isActive} 
+          style={[ 
+            styles.card, { backgroundColor: isActive ? "#f7f7fc" : item.backgroundColor },]}>
+            <View
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={styles.firsttext}>{item.cardname}</Text>
+            </View>
+            <View style={styles.cardtext}>
+
+            <TouchableOpacity style={styles.textButtons}>
+              <Text style={styles.textCard}>{item.text}</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.textButtons}>
+              <Text style={styles.textCard}>{item.text2}</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.textButtons}>
+              <Text style={styles.textCard}>{item.text3}</Text>
+              </TouchableOpacity>
+              
+              <TouchableOpacity style={styles.textButtons}>
+              <Text style={styles.textCard}>{item.text4}</Text>
+              </TouchableOpacity>
+              {/* <Text>{item.May}</Text>
+              <Text>{item.Jun}</Text>
+              <Text>{item.Jul}</Text>
+              <Text>{item.Aug}</Text>
+              <Text>{item.Sep}</Text>
+              <Text>{item.Oct}</Text>
+              <Text>{item.Nov}</Text>
+              <Text>{item.Dec}</Text>*/}
+        </View>
+
+          </TouchableOpacity>
+      </ScaleDecorator>
+    );
+  };
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1, backgroundColor: "#f7f7fc" }}>
+      <SafeAreaProvider>
+        <DraggableFlatList
+          data={data}
+          onDragEnd={({ data }) => setData(data)}
+          contentContainerStyle={{ paddingBottom:100 }}
+          keyExtractor={(item) => item.id}
+          renderItem={renderItem}
+        />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: "#f7f7fc",
+  },
+  rowItem: {
+    height: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    
+  },
+  scrollview: {
+    flex: 1,
+    backgroundColor: "#fff",
+    position: "relative",  
+  },
+  card: {
+    marginHorizontal: "auto",
+    width: width / 1.05,
+    borderRadius: 12,
+    backgroundColor: 'white',
+    height: 155,
+    elevation: 10,
+    shadowColor: "#c0c0c0",
+    paddingHorizontal: 12,
+    marginVertical: -8,
+    marginLeft:10,
+  },
+  firsttext: {
+    paddingTop: 25,
+    paddingBottom: 10,
+    paddingHorizontal: 15,
+    fontSize: 18,
+    fontWeight: "900",
+    margin: 2,
+  },
+  cardtext:{
+    fontWeight: "bold",
+    color: "black",
+    paddingVertical: 15,
+    flexDirection: 'row',  
+    justifyContent: 'space-evenly',
+    paddingHorizontal: 5,
+  },
+  textButtons:{
+    backgroundColor:'#F0F2FA',
+    borderRadius:12,
+    width:65,
+    height:45,
+    elevation: 2,
+  },
+  textCard:{
+  fontSize:15,
+  fontWeight:'bold',
+  textAlign:'center',
+  color:'#425CE2',
+  top:10,
+  },
+});
--- Cards/Main/Pages/Expense.js
+++ Cards/Main/Pages/Expense.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Image, SafeAreaView,Button,Alert, TouchableHigh
 import React, { useState } from "react";
 
 import Expense from './Icons/Expense.png';
+import DragDropExpense  from './DragDropExpense';
 
 
 export default function expense({text , onPress }) { 
@@ -12,6 +13,10 @@ export default function expense({text , onPress }) {
       <SafeAreaView style={styles.body}>
         <Image source={Expense} style={styles.expenseBackground} />
         <Text style={styles.pageMainTitle}>קבלות הוצאה</Text>
+        <View style={styles.background}>
+        <DragDropExpense/>
+
+        </View>
       </SafeAreaView>
     );
 }
@@ -19,7 +24,7 @@ export default function expense({text , onPress }) {
   const styles = StyleSheet.create({
     expenseBackground:{
       width:'100%',
-      height:260,
+      height:280,
     },
     body: {
       width: 390,
@@ -31,108 +36,16 @@ export default function expense({text , onPress }) {
         fontWeight:'bold',
         textAlign:'center',
         top:0,
+        left:10,
     },
-    pageTitle: {
-      fontSize:23,
-      fontWeight:'bold',
-      top:13,
-      right:8,
+    background:{
+        backgroundColor:'#f7f7fc',
+        width:'100%',
+        height:'100%',
+        left:1.5,
+        top:5,     
+        elevation: 4,
+        borderRadius:12,
     },
-    PageSubTitle: {
-      fontSize:17,
-      fontWeight:'bold',
-      top:25,    
-      right:8,
-    },
-    inputMail:{
-      fontSize: 18,
-      textAlign: 'right',
-      position: 'absolute',
-      width: 350,
-      height: 35,
-      left: 30,
-      top: 395,
-      elevation: 4,
-      borderRadius:8,
-      backgroundColor: 'white',    
-      shadowColor: 'grey',
-      shadowOffset: {
-      height: 1.4,
-      width: 0.1
-      }
-  },
-  text:{
-    fontSize: 14,
-    textAlign: 'right',
-    fontWeight:'bold',
-    top:80,
-    right:8,
-  },
-  socialTitle:{
-    fontSize: 20,
-    textAlign: 'right',
-    fontWeight:'bold',
-    top:95,  
-  },
-  socialIcons:{
-    width:30,
-    height:30,
-    marginTop:65,
-    marginRight:200,
-  },
-  row:{
-    flexDirection: 'row',  
-    justifyContent: 'space-evenly',
-    paddingHorizontal: 190,
-    },
-  text1:{
-    fontSize: 15,
-    textAlign: 'right',
-    fontWeight:'bold',
-    top:30,
-  },
-  codeBoard:{
-   width:160,
-   height:55,
-   backgroundColor:'#7471F2',
-   borderRadius:16,
-   top:65,
-   left:210,
-  },
-  codeBoardCopy:{
-    color:'white',
-    fontWeight:'bold',
-    fontSize:22,
-    textAlign:'center',
-    top:10,
-   },
-   codeBoardTitle:{
-    color:'black',
-    fontWeight:'bold',
-    fontSize:17,
-    top:55,
-    right:62,
-   },
-  clickedBoard:{
-    width:160,
-    height:55,
-    backgroundColor:'#7471F2',
-    borderRadius:16,
-    left:20,  
-    top:-13,
-   },
-   clicked:{
-     color:'white',
-     fontWeight:'bold',
-     fontSize:22,
-     textAlign:'center',
-     top:10,
-    },
-    clickedBoardTitle:{
-        color:'black',
-        fontWeight:'bold',
-        fontSize:17,
-        right:204,
-        top:-22,
-    },
+    
   });
