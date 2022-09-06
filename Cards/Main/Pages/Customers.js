ab034b314e1a23dc6fb84c3fc58e912959fb2928
idan <72699248+idanbe11@users.noreply.github.com>
2022-09-06 23:20:42 +0300

    commit

--- Cards/Main/Pages/Customers.js
+++ Cards/Main/Pages/Customers.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView,Button,Alert, TouchableHighlight,TouchableWithoutFeedback, TouchableOpacity , TextInput, ScrollView} from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView,Button,Alert, TouchableHighlight,TouchableWithoutFeedback, TouchableOpacity , TextInput, ScrollView,onPress} from 'react-native';
 import React, { useState } from "react";
 import { Table, TableWrapper, Cell, Row, Rows, Col, Cols } from 'react-native-table-component';
 
@@ -18,7 +18,7 @@ class Customers extends React.Component {
         const tableData = [];
         for (let i = 0; i < 6; i += 1) {
           const rowData = [];
-          for (let j = 0; j < 9; j += 1) {
+          for (let j = 0; j < 6; j += 2) {
             rowData.push(`${i}${j}`);
           }
           tableData.push(rowData);
@@ -28,25 +28,38 @@ class Customers extends React.Component {
     return (
       <SafeAreaView style={styles.body}>
         <Image source={customers} style={styles.CustomersBackground} />
+        
             <View style={styles.container}>
                 <View style={styles.subContainer}>
                     <Text style={styles.mainTitle}>לקוחות שלי</Text>
                 </View>
-                <View style={styles.container1}>
+
+                <TouchableOpacity onPress={onPress}>
+                    <View style={styles.filteringButton}>
+                        <Text style={styles.filteringButtonText}>סינון</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={onPress}>
+                    <View style={styles.addCustomersButton}>
+                        <Text style={styles.addCustomersButtonText}>הוסף לקוח</Text>
+                    </View>
+                </TouchableOpacity>
+
+        <View style={styles.container1}>
         <ScrollView horizontal={true}>
           <View>
-            <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+            <Table>
               <Row data={state.tableHead} widthArr={state.widthArr} style={styles.header} textStyle={styles.text}/>
             </Table>
             <ScrollView style={styles.dataWrapper}>
-              <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+              <Table>
                 {
                   tableData.map((rowData, index) => (
                     <Row
                       key={index}
                       data={rowData}
                       widthArr={state.widthArr}
-                      style={[styles.row, index%2 && {backgroundColor: '#F7F6E7'}]}
+                      style={[styles.row, index%2 && {borderTopWidth: 8,borderColor: 'white',}]}
                       textStyle={styles.text}
                     />
                   ))
@@ -98,26 +111,65 @@ export default (Customers);
         textAlign:'center',
         color:'white',
         marginTop:10,
+        fontWeight:'700',
     },
 
     container1: { 
         flex: 1, 
         padding: 16, 
-        paddingTop: 30, 
+        paddingTop: 80, 
     },
   header: { 
     height: 50, 
-    backgroundColor: 'white' 
+    backgroundColor: '#e9f0f7' ,
 },
   text: { 
     textAlign: 'center', 
-    fontWeight: '100' 
+    fontWeight: '700' ,
+    color:'black',
+    fontSize:18,
 },
   dataWrapper: { 
-    marginTop: -1 
+    marginTop: -1 ,
 },
   row: { 
     height: 40, 
-    backgroundColor: 'white', 
-}
+    borderTopWidth: 8,
+    borderColor: 'white',
+},
+  filteringButton:{
+    width:40,
+    height:40, 
+    top:40,
+    position:'absolute',
+    marginLeft:52,
+},
+filteringButtonText:{
+    color:'black',
+    fontSize:18,
+    fontWeight:'700',
+},
+addCustomersButton:{
+    top:40,
+    position:'absolute',
+    width:140,
+    height:40, 
+    marginLeft:232,
+    borderRadius:22,
+    backgroundColor: "#7471F2",
+    shadowColor: "#a9a9a9",
+    shadowOffset: {
+      width: 2,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+},
+addCustomersButtonText:{
+    textAlign:'center',
+    color:'white',
+    fontSize:18,
+    fontWeight:'700',
+    top:5,
+},
   });
